e9aa093d65f1d538b234e2447442fcfe597bc1fe
Francisco Pontes de Lima Neto <franciscopontes@MacBook-Air-de-Pontes.local>
2023-12-05 15:41:47 -0300

    create aerofunction newTodo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -26,11 +26,23 @@ function App() {
     }
   ]);
 
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+      id: Math.floor(Math.random()* 10000),
+      text,
+      category,
+      isCompleted: false,
+    },
+  ];
+  }
+
   return <div className="app">
     <h1>Lista de Tarefas</h1>
     <div className="todo-list">
       {todos.map((todo) => (
-        <Todo todo={todo} />
+        <Todo key={todo.id} todo={todo} />
       ))}
     </div>
     <TodoForm />
