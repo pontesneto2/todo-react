b322b533f3a3529a3152fcd71eaaab56f6491f18
Francisco Pontes de Lima Neto <franciscopontes@MacBook-Air-de-Pontes.local>
2023-12-07 12:44:17 -0300

    finally component search

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -27,6 +27,8 @@ function App() {
     }
   ]);
 
+  const [search, setSearch] = useState("");
+
   const addTodo = (text, category) => {
     const newTodos = [
       ...todos,
@@ -57,9 +59,9 @@ function App() {
   return (
     <div className="app">
     <h1>Lista de Tarefas</h1>
-    <Search/>
+    <Search search={search} setSearch={setSearch}/>
     <div className="todo-list">
-      {todos.map((todo) => (
+      {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
         <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
       ))}
     </div>
